28304f866f7cb2fb76fe18de0af2c5eeda44634e
Ivan <magistro999@gmail.com>
2021-02-26 19:19:52 +0300

    Updated FrontController with new get and post methods and created ClientService (still needs updates). Was trying to build some tests, but nothing worked out.

--- Maven project/src/main/java/com/services/ClientService.java
+++ Maven project/src/main/java/com/services/ClientService.java
@@ -1,18 +1,62 @@
 package com.services;
 
-import com.dataSets.Client;
+import com.entities.Client;
 import com.repositories.ClientRepository;
+import com.resources.ClientResource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service
+@Component
 public class ClientService {
     @Autowired
-    private ClientRepository clientRepository;
+    private ClientRepository repository;
 
-    public List<Client> list() {
-        return clientRepository.findAll();
+    public ClientService(ClientRepository clientRepository) {
+        this.repository = clientRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Client getClientByLogin(String login) {
+        List<Client> found = repository.findByLogin(login);
+        if (found.isEmpty()){
+            return null;
+        }
+        else {
+            return found.get(0);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Client getClientById(Long id) {
+        Optional<Client> res = repository.findById(id);
+        if (res.isEmpty()){
+            return null;
+        }
+        else {
+            return res.get();
+        }
+    }
+
+    public Long add(ClientResource client) {
+        Client created = new Client(client.getLogin(),
+                client.getName(), client.getPassword(), client.getBalance());
+        created = repository.save(created);
+        return created.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public boolean exists(ClientResource client) {
+        List<Client> found = repository.findByLoginAndNameAndPasswordAndBalance(client.getLogin(),
+                client.getName(), client.getPassword(), client.getBalance());
+        if (found.isEmpty()){
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
